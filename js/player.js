2f2fc36e8b77f3b03bfd1544b02a2a7acd41fd88
Geison Biazus <geisonbiazus@gmail.com>
2015-07-28 23:14:12 -0300

    use collideWorldBounds to prevent player from going out of the screen

--- js/player.js
+++ js/player.js
@@ -2,22 +2,19 @@ Player = function (game, x, y) {
   this.game = game;
   this.sprite = game.add.sprite(x, y, 'player');
   this.sprite.anchor.setTo(0.5, 0.5);
-  game.physics.arcade.enable(this.sprite);
+  this.game.physics.arcade.enable(this.sprite);
+  this.sprite.body.collideWorldBounds = true;
 };
 
 Player.UP = -1;
 Player.DOWN = 1;
 
 Player.prototype.moveUp = function () {
-  if (!this.touchingTop()) {
-    this.move(Player.UP);
-  }
+  this.move(Player.UP);
 };
 
 Player.prototype.moveDown = function () {
-  if (!this.touchingBottom()) {
-    this.move(Player.DOWN);
-  }
+  this.move(Player.DOWN);
 };
 
 Player.prototype.move = function (direction) {
@@ -27,11 +24,3 @@ Player.prototype.move = function (direction) {
 Player.prototype.stop = function () {
   this.sprite.body.velocity.y = 0;
 };
-
-Player.prototype.touchingBottom = function () {
-  return (this.sprite.y + this.sprite.height / 2) >= this.game.world.height;
-}
-
-Player.prototype.touchingTop = function () {
-  return (this.sprite.y - this.sprite.height / 2) <= 0;
-}
